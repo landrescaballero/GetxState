40eb722605f609db8e2991f045e00f6dd02ceab0
landrescaballero <cleonard@uninorte.edu.co>
2023-08-30 10:52:44 -0500

    Finiquitado

--- lib/ui/widgets/w3.dart
+++ lib/ui/widgets/w3.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/count_controller.dart';
 
 class W3 extends StatelessWidget {
   const W3({super.key});
 
   @override
   Widget build(BuildContext context) {
+    CountController controller = Get.find();
     return Container(
       color: Colors.yellowAccent,
-      child: const Column(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Value'),
+          SizedBox(
+            width: 60,
+            child:Center(
+              child:Obx(() => Text(controller.count.toString()))
+              )
+          )
         ],
       ),
     );
